183613b6479d5de8ce164af692ba6644af0ffc5b
SC <jesuscastellanospaez@hotmail.com>
2019-12-18 11:37:43 -0500

    Clase # 23 Se cambian las funciones por clases en el proyecto JS

--- CursoDeProgramacionOrientadaAObjetosPOO/CursoPOOUber/JS/Account.js
+++ CursoDeProgramacionOrientadaAObjetosPOO/CursoPOOUber/JS/Account.js
@@ -1,7 +1,12 @@
-function Account(name, document){
-    this.id;
-    this.name = name;
-    this.document = document;
-    this.mail;
-    this.password;
+class Account {
+
+    constructor(name, document) {
+        this.id;
+        this.name = name;
+        this.document = document;
+        this.email;
+        this.password;
+    }
+    
+
 }
--- CursoDeProgramacionOrientadaAObjetosPOO/CursoPOOUber/JS/index.js
+++ CursoDeProgramacionOrientadaAObjetosPOO/CursoPOOUber/JS/index.js
@@ -1,3 +1,3 @@
-var car = new Car("AW453", new Account("Andres Herrera"))
+var car = new Car("AW453", new Account("Andres Herrera", "AND 123"))
 car.passenger = 4;
 car.printDataCar()
